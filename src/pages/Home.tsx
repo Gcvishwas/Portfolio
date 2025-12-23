4a6d0d7bd2408aa709b50f829cfec12ef029f28b
Gcvishwas <gcvishwas17@gmail.com>
2025-12-23 18:49:59 +0545

    Initial Home component created

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import ThemeToggle from "../Components/ThemeToggle";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Toggle */}
+      <ThemeToggle />
+      {/* Background Effects*/}
+      {/* Navbar */}
+      {/* Main */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Home;
