50c10837df6ef2c6fd50dd4340901aee61b20c43
Gcvishwas <gcvishwas17@gmail.com>
2025-12-23 18:45:34 +0545

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-@import "tailwindcss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
