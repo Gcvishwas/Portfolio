1ff43470be0e1899eadcae6ba242385946065682
Gcvishwas <gcvishwas17@gmail.com>
2025-12-24 22:28:21 +0545

    Add StarBackground

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import StarBackground from "../Components/StarBackground";
 import ThemeToggle from "../Components/ThemeToggle";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
       {/* Toggle */}
       <ThemeToggle />
       {/* Background Effects*/}
+      <StarBackground />
       {/* Navbar */}
       {/* Main */}
       {/* Footer */}
