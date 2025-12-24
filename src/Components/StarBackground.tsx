8bc7aea5d3fa230905adf581fd40a45b99e48006
Gcvishwas <gcvishwas17@gmail.com>
2025-12-24 22:57:59 +0545

    Add Meteors

--- src/Components/StarBackground.tsx
+++ src/Components/StarBackground.tsx
@@ -8,12 +8,27 @@ type Star = {
   opacity: number;
   animationDuration: number;
 };
+type Meteor = {
+  id: number;
+  size: number;
+  x: number;
+  y: number;
+  delay: number;
+  animationDuration: number;
+};
 
 const StarBackground = () => {
   const [stars, setStars] = useState<Star[]>([]);
-
+  const [meteors, setMeteors] = useState<Meteor[]>([]);
   useEffect(() => {
     generateStars();
+    generateMeteors();
+
+    const handleResize = () => {
+      generateStars();
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const generateStars = () => {
@@ -34,6 +49,22 @@ const StarBackground = () => {
     }
     setStars(newStars);
   };
+
+  const generateMeteors = () => {
+    const numberofMeteors = 4;
+    const newMeteors: Meteor[] = [];
+    for (let i = 0; i < numberofMeteors; i++) {
+      newMeteors.push({
+        id: i,
+        size: Math.random() * 2 + 1,
+        x: Math.random() * 100,
+        y: Math.random() * 20,
+        delay: Math.random() * 15,
+        animationDuration: Math.random() * 3 + 3,
+      });
+    }
+    setMeteors(newMeteors);
+  };
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
       {stars.map((star) => (
@@ -47,6 +78,20 @@ const StarBackground = () => {
             top: star.y + "%",
             opacity: star.opacity,
             animationDuration: star.animationDuration + "s",
+          }}
+        ></div>
+      ))}
+      {meteors.map((meteor) => (
+        <div
+          key={meteor.id}
+          className="meteor animate-meteor"
+          style={{
+            width: meteor.size * 50 + "px",
+            height: meteor.size * 2 + "px",
+            left: meteor.x + "%",
+            top: meteor.y + "%",
+            animationDelay: meteor.delay + "ms",
+            animationDuration: meteor.animationDuration + "s",
           }}
         ></div>
       ))}
