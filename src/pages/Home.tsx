debb485e9ae4eb05ae7dfcb17f4f0810ba3e74e3
Gcvishwas <gcvishwas17@gmail.com>
2025-12-30 21:06:07 +0545

    Add Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import About from "../Components/About";
+import Footer from "../Components/Footer";
 import Hero from "../Components/Hero";
 import Navbar from "../Components/Navbar";
 import Projects from "../Components/Projects";
@@ -23,6 +24,7 @@ const Home = () => {
         <Projects />
       </main>
       {/* Footer */}
+      <Footer />
     </div>
   );
 };
