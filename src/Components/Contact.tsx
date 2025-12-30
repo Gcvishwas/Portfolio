a2e5b671030290ad9b01e9256c4300d15cb4bf6e
Gcvishwas <gcvishwas17@gmail.com>
2025-12-30 21:11:57 +0545

    Contact component initial version

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,135 @@
+import { Mail, Map, Phone, Send } from "lucide-react";
+import { cn } from "../lib/utils";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="py-24 px-4 bg-background/30 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Chiya<span> Guff?</span>
+        </h2>
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Got anything to discuss? Feel free to reach out. I am open to
+          discussing about any quirky topic.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+            <div className="space-y-6  space-x-1 flex  md:flex-col md:px-26 flex-row justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/20">
+                  <Mail className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Email</h4>
+                  <a
+                    href="mailto:gcvishwas17@gmail.com"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    gcvishwas17@gmail.com
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/20">
+                  <Phone className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Phone</h4>
+                  <a
+                    href="tel:+977 9866052973"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    +977 9866052973
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/20">
+                  <Map className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Address</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                    Kathmandu, Nepal
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-card p-8 rounded-lg shadow-xs">
+            <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
+            <form
+              action="mailto:gcvishwas17@gmail.com"
+              method="POST"
+              encType="text/plain"
+              className="space-y-6"
+            >
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  className="w-full px-4 py-3 border border-input bg-background focus:outline-hidden
+                 "
+                  placeholder="Enter Name..."
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="w-full px-4 py-3 border border-input bg-background focus:outline-hidden
+                 "
+                  placeholder="Your mail..."
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  Your Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  required
+                  className="w-full px-4 py-3 border border-input bg-background focus:outline-hidden
+                  resize-none
+                 "
+                  placeholder="Enter your Message"
+                />
+              </div>
+              <button
+                type="submit"
+                className={cn(
+                  "cosmic-button w-full flex items-center justify-center gap-2"
+                )}
+              >
+                <Send size={16} />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
